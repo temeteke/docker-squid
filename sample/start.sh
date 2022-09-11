2bf2f9c94fdc1fe7431c86cd0960cbf32ca06e11
temeteke <temeteke@gmail.com>
2022-09-11 17:36:02 +0900

    Change start script not to create tls key

--- sample/start.sh
+++ sample/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker build -t squid ..
-touch squid.crt
-docker run -d --name squid -p 3128:3128 -v $PWD/squid.conf:/etc/squid/squid.conf -v $PWD/squid.crt:/etc/squid/squid.crt squid
+openssl req -x509 -nodes -new -keyout squid.key -out squid.crt -days 365 -subj /CN=localhost
+docker run -d --name squid -p 3128:3128 -v $PWD/squid.conf:/etc/squid/squid.conf -v $PWD/squid.key:/etc/squid/squid.key -v $PWD/squid.crt:/etc/squid/squid.crt squid
