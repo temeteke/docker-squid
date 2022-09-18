31c030b0fc1b2de48ffb67561056ee0634387294
temeteke <temeteke@gmail.com>
2022-09-18 14:50:26 +0900

    Change command line option

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ rm -rf /var/cache/squid/ssl_db
 /usr/lib/squid/security_file_certgen -c -s /var/cache/squid/ssl_db -M 4MB
 chown squid:squid -R /var/cache/squid/ssl_db
 
-exec squid -N
+exec squid -zN
