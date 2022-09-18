71699d26d22ec06ecb1c84248294aac9b3f7ccb6
temeteke <temeteke@gmail.com>
2022-09-18 15:04:24 +0900

    Change a script to change permission of directory

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ chown squid:squid /dev/stdout
 
 rm -rf /var/cache/squid/ssl_db
 /usr/lib/squid/security_file_certgen -c -s /var/cache/squid/ssl_db -M 4MB
-chown squid:squid -R /var/cache/squid/ssl_db
+chown squid:squid -R /var/cache/squid
 
 exec squid -zN
