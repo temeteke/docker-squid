ce46ca51633fb9b2d3d0f549ad5de9d3c9962fb5
temeteke <temeteke@gmail.com>
2022-09-18 15:30:05 +0900

    Change start script

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-chown squid:squid /dev/stdout
-
 rm -rf /var/cache/squid/ssl_db
 /usr/lib/squid/security_file_certgen -c -s /var/cache/squid/ssl_db -M 4MB
 chown squid:squid -R /var/cache/squid
@@ -8,5 +6,7 @@ chown squid:squid -R /var/cache/squid
 if [ ! -d /var/cache/squid/00 ]; then
 	squid -zN
 fi
+
+chown squid:squid /dev/stdout
 
 exec squid -N
