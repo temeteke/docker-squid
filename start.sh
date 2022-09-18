99f00fb73896acb1b7c1e4fc6f05658322c24268
temeteke <temeteke@gmail.com>
2022-09-18 15:37:41 +0900

    Change script

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-rm -rf /var/cache/squid/ssl_db
-/usr/lib/squid/security_file_certgen -c -s /var/cache/squid/ssl_db -M 4MB
-chown squid:squid -R /var/cache/squid
-
+chown squid:squid /var/cache/squid
 if [ ! -d /var/cache/squid/00 ]; then
 	squid -zN
 fi
+
+rm -rf /var/cache/squid/ssl_db
+/usr/lib/squid/security_file_certgen -c -s /var/cache/squid/ssl_db -M 4MB
+chown squid:squid -R /var/cache/squid/ssl_db
 
 chown squid:squid /dev/stdout
 
