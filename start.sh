c80026e75d6fdd49be505e63617e91fac38ffc51
temeteke <temeteke@gmail.com>
2022-09-18 15:15:36 +0900

    Change script to create cache directory

--- start.sh
+++ start.sh
@@ -5,4 +5,8 @@ rm -rf /var/cache/squid/ssl_db
 /usr/lib/squid/security_file_certgen -c -s /var/cache/squid/ssl_db -M 4MB
 chown squid:squid -R /var/cache/squid
 
-exec squid -zN
+if [ ! -d /var/cache/squid/00 ]; then
+	squid -z
+fi
+
+exec squid -N
